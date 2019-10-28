65f2960a2db5db97c25caaf2d28df42b5c247fd9
lbarthon <lsbarthonet2@gmail.com>
2019-10-28 13:10:31 +0100

    feat(parser): add first try on parser

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/MonsieurTa/n-puzzle/parser"
 )
 
 func main() {
-	parser.Salut()
+	tab, err := parser.Parse()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+	} else {
+		for i := 0; i < len(tab); i++ {
+			for j := 0; j < len(tab[i]); j++ {
+				fmt.Printf("x:%d - y:%d = %d\n", i, j, tab[i][j])
+			}
+		}
+	}
 }
--- parser/parser.go
+++ parser/parser.go
@@ -1,5 +1,62 @@
 package parser
 
-func Salut() {
-	print("Hello")
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func Parse() ([][]int, error) {
+	reader := bufio.NewReader(os.Stdin)
+	sizeStr, err := reader.ReadString('\n')
+	if err != nil {
+		return nil, err
+	}
+	sizeStr = strings.TrimSpace(sizeStr)
+	size, err := strconv.Atoi(sizeStr)
+	if err != nil {
+		return nil, err
+	}
+	if size <= 1 {
+		return nil, errors.New("n-puzzle: size must be greater than 1")
+	}
+	tab := make([][]int, size)
+	for i := 0; i < size; i++ {
+		str, err := reader.ReadString('\n')
+		if err != nil {
+			return nil, err
+		}
+		numbers, err := checkValidity(str, size, i)
+		if err != nil {
+			return nil, err
+		}
+		tab[i] = numbers
+	}
+	return tab, err
+}
+
+func checkValidity(str string, size int, line int) ([]int, error) {
+	numbers := make([]int, size)
+	var hashtagIndex int = strings.IndexByte(str, '#')
+	if hashtagIndex > 0 {
+		str = str[:hashtagIndex]
+	}
+	words := strings.Fields(str)
+	for i := 0; i < len(words); i++ {
+		nbr, err := strconv.Atoi(words[i])
+		if err != nil {
+			return nil, err
+		}
+		if i >= size {
+			return nil, fmt.Errorf("n-puzzle: line %d has %d numbers instead of %d", line+1, len(words), size)
+		}
+		numbers[i] = nbr
+	}
+	if len(words) < size {
+		return nil, fmt.Errorf("n-puzzle: line %d has %d numbers instead of %d", line+1, len(words), size)
+	}
+	return numbers, nil
 }
