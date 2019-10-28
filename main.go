1f33e2f9ca67e4ef9447c66466757df8f6cb8a89
Ta William <wta@student.42.fr>
2019-10-28 15:40:45 +0100

    chore: empty main

--- main.go
+++ main.go
@@ -1,21 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"os"
-
-	"github.com/MonsieurTa/n-puzzle/parser"
-)
-
 func main() {
-	tab, err := parser.Parse()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
-	} else {
-		for i := 0; i < len(tab); i++ {
-			for j := 0; j < len(tab[i]); j++ {
-				fmt.Printf("x:%d - y:%d = %d\n", i, j, tab[i][j])
-			}
-		}
-	}
 }
