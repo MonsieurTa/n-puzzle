7f39d7b400ffed452f1a9ab7eaddf5ca6ba7a149
William TA <wta@e1r7p9.42.fr>
2019-10-31 18:49:07 +0100

    feat: forward wip on new a star

--- algo/astar.go
+++ algo/astar.go
@@ -1,230 +1,124 @@
 package algo
 
 import (
-	"crypto/md5"
-	"encoding/hex"
-	"fmt"
-	"math"
+	"container/heap"
 
-	"github.com/MonsieurTa/n-puzzle/utils"
+	"github.com/MonsieurTa/n-puzzle/state"
 )
 
 const MaxUint = ^uint(0)
 const MaxInt = int(MaxUint >> 1)
 
+//PriorityQueue ...
+type PriorityQueue []*Node
+
+//Len return the len of the prority queue
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+//Less return priority condition
+func (pq PriorityQueue) Less(i, j int) bool { return pq[i].f < pq[j].f }
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = j
+	pq[j].index = i
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*Node)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil  // avoid memory leak
+	item.index = -1 // for safety
+	*pq = old[0 : n-1]
+	return item
+}
+
+// update modifies the priority and value of an Item in the queue.
+func (pq *PriorityQueue) update(item *Node, g, f, h int) {
+	item.g = g
+	item.f = f
+	item.h = h
+	heap.Fix(pq, item.index)
+}
+
 //Node ...
 type Node struct {
-	Parent    *Node
-	X, Y      int
-	Hash      string
-	State     [][]int
-	Heuristic int
-	Fscore    int
+	index  int
+	parent *Node
+	state  state.State
+	g      int
+	f      int
+	h      int
+}
+
+func (n Node) copy() *Node {
+	return &Node{
+		parent: n.parent,
+		state:  n.state,
+		g:      n.g,
+		f:      n.f,
+		h:      n.h,
+	}
+}
+
+func (n Node) getChild() []*Node {
+	childState := n.state.GetSurrounding()
+	child := []*Node{}
+	for _, state := range childState {
+		child = append(child, &Node{
+			parent: &n,
+			state:  *state,
+			g:      n.g + 1,
+			f:      0,
+			h:      0,
+		})
+	}
 }
 
 //Algo ...
 type Algo struct {
-	Goal     *Node
-	CameFrom []*Node
-	GScore   map[string]int
-	FScore   map[string]int
+	start state.State
+	goal  state.State
 }
 
-//Return ...
-type Return struct {
-	TimeComplex int
-	SizeComplex int
-	Nodes       []*Node
+func (a Algo) isGoal(state state.State) bool {
+	return a.goal.Str == state.Str
 }
 
-//HashState ...
-func HashState(State [][]int) string {
-	base := ""
-	for _, row := range State {
-		base += fmt.Sprint(row)
-	}
-	hash := md5.Sum([]byte(base))
-	res := hex.EncodeToString(hash[:])
-	return res
+// Init set start and goal state to algo struct
+func (a *Algo) Init(start, goal state.State) {
+	a.start, a.goal = start, goal
 }
 
-func GetRootPos(board [][]int) (int, int) {
-	for Y, row := range board {
-		for X, col := range row {
-			if col == 0 {
-				return X, Y
-			}
-		}
-	}
-	return -1, -1
-}
-
-func clone(a *Node, b *Node) {
-	b.Heuristic = a.Heuristic
-	b.Hash = a.Hash
-	b.Parent = a.Parent
-	b.State = utils.DeepCopy(a.State)
-	b.X = a.X
-	b.Y = a.Y
-}
-
-func shiftTile(newX int, newY int, elem *Node, size int) *Node {
-	var cpy Node = Node{}
-
-	if newX < 0 || newY < 0 || newX >= size || newY >= size {
-		return nil
-	}
-	clone(elem, &cpy)
-	cpy.Parent = nil
-	tmp := cpy.State[cpy.Y][cpy.X]
-	cpy.State[cpy.Y][cpy.X] = cpy.State[newY][newX]
-	cpy.State[newY][newX] = tmp
-	cpy.X = newX
-	cpy.Y = newY
-	cpy.Hash = HashState(cpy.State)
-	return &cpy
-}
-
-func processNode(elem *Node, a *Algo, h []func(*Node, *Node) int) []*Node {
-	child := make([]*Node, 0)
-	size := len(elem.State)
-	shifts := []*Node{
-		shiftTile(elem.X, elem.Y-1, elem, size),
-		shiftTile(elem.X, elem.Y+1, elem, size),
-		shiftTile(elem.X-1, elem.Y, elem, size),
-		shiftTile(elem.X+1, elem.Y, elem, size),
-	}
-	for _, item := range shifts {
-		if item != nil {
-			item.Heuristic = applyHeuristics(item, a.Goal, h)
-			child = append(child, item)
-			if _, ok := a.GScore[item.Hash]; !ok {
-				a.GScore[item.Hash] = MaxInt
-			}
-			if _, ok := a.FScore[item.Hash]; !ok {
-				a.GScore[item.Hash] = MaxInt
-			}
-		}
-	}
-	return child
-}
-
-func reconstructPath(cameFrom []*Node, current *Node) []*Node {
-	node := current
-	totalPath := []*Node{node}
-	for node.Parent != nil {
-		node = node.Parent
-		totalPath = append([]*Node{node}, totalPath...)
-	}
-	return totalPath
-}
-
-//Init ...
-func (a *Algo) Init(size int, goal *Node) {
-	totalSize := size * size
-	a.CameFrom = make([]*Node, totalSize)
-	a.GScore = make(map[string]int)
-	a.FScore = make(map[string]int)
-	a.Goal = goal
-}
-
-func binarySearch(list []*Node, x int) int {
-	var ret int
-	low := 0
-	high := len(list) - 1
-
-	for low <= high {
-		m := int(math.Floor(float64(low+high) / 2))
-		ret = m
-		if list[m].Fscore < x {
-			low = m + 1
-		} else if list[m].Fscore > x {
-			high = m - 1
-		} else {
-			return m
-		}
-	}
-	if len(list) == 0 {
-		return 0
-	} else if list[ret].Fscore < x {
-		return ret + 1
-	}
-	return ret
-}
-
-func applyHeuristics(node1 *Node, node2 *Node, arr []func(*Node, *Node) int) int {
-	ret := 0
-	for _, h := range arr {
-		ret += h(node1, node2)
-	}
-	return ret
-}
-
-func isSort(list []*Node) bool {
-	for i := 0; i < len(list)-1; i++ {
-		if list[i].Fscore > list[i+1].Fscore {
-			return false
-		}
-	}
-	return true
-}
-
-func getLowestCost(mapper map[string]*Node, goal *Node) *Node {
-	var res *Node
-	score := MaxInt
-	for _, item := range mapper {
-		if item.Heuristic < score {
-			score = item.Heuristic
-			res = item
-		}
-	}
-	delete(mapper, res.Hash)
-	return res
-}
-
-func (a *Algo) AStar(start *Node, goal *Node, greedy bool, h []func(*Node, *Node) int) Return {
-	var elem *Node
-	// Initializing return
-	var ret Return
-	ret.Nodes = nil
-
-	// sortedNode := []*Node{start}
-	openSet := map[string]*Node{start.Hash: start}
-	closedSet := map[string]*Node{}
-	a.GScore[start.Hash] = 0
-	start.Fscore = applyHeuristics(start, goal, h)
+func (a *Algo) AStar(h func(a, b *state.State) int) {
+	openSet := PriorityQueue{&Node{
+		state: a.start,
+		f:     h(&a.start, &a.goal),
+		g:     0,
+		h:     h(&a.start, &a.goal),
+	}}
+	heap.Init(&openSet)
+	closedSet := make(map[string]*Node, 0)
 	for len(openSet) > 0 {
-		elem = getLowestCost(openSet, goal)
-		// elem, sortedNode = sortedNode[0], sortedNode[1:]
-		// delete(openSet, elem.Hash)
-		if elem.Hash == goal.Hash {
-			ret.Nodes = reconstructPath(a.CameFrom, elem)
-			ret.SizeComplex = len(openSet) + len(closedSet)
-			ret.TimeComplex = len(closedSet)
-			return ret
+		elem := heap.Pop(&openSet).(*Node)
+		if a.isGoal(elem.state) {
+			return
 		}
-		closedSet[elem.Hash] = elem
-		child := processNode(elem, a, h)
+		closedSet[elem.state.Str] = elem
+		child := elem.getChild()
 		for _, children := range child {
-			if _, ok := closedSet[children.Hash]; ok {
+			if _, ok := closedSet[children.state.Str]; ok {
 				continue
 			}
-			currGScore := a.GScore[elem.Hash] + 1
-			if greedy == true || currGScore < a.GScore[children.Hash] {
-				children.Parent = elem
-				a.GScore[children.Hash] = currGScore
-				if greedy {
-					children.Fscore = children.Heuristic
-				} else {
-					children.Fscore = a.GScore[children.Hash] + children.Heuristic
-				}
-				if _, ok := openSet[children.Hash]; !ok {
-					openSet[children.Hash] = children
-					// i := binarySearch(sortedNode, children.Fscore)
-					// sortedNode = append(sortedNode[:i], append([]*Node{children}, sortedNode[i:]...)...)
-				}
-			}
+
 		}
 	}
-	return ret
 }
--- main.go
+++ main.go
@@ -11,47 +11,30 @@ import (
 
 func main() {
 	var npuzzle parser.Data
-	parser.ParseArgs(&npuzzle)
+	var a algo.Algo
 
+	parser.ParseArgs(&npuzzle)
 	start, err := parser.Parse(&npuzzle)
 	if err != nil {
 		fmt.Fprint(os.Stderr, err, "\n")
 		return
 	}
-
-	var a algo.Algo
 	board := npuzzle.Goal(len(start))
-	x, y := algo.GetRootPos(start)
-	root := algo.Node{
-		Parent: nil,
-		X:      x, Y: y,
-		Hash:  algo.HashState(start),
-		State: start,
-	}
-	x, y = algo.GetRootPos(board)
-	goal := algo.Node{
-		Parent: nil,
-		X:      x, Y: y,
-		Hash:  algo.HashState(board),
-		State: board,
-	}
+	a.Init(start, board)
+
 	fmt.Printf("Initial heuristic scores:\n")
 	for key, value := range algo.Heuristics {
 		fmt.Printf("- %s: %d\n", key, value(&root, &goal))
 	}
-	a.Init(len(board), &goal)
 	if gen.IsSolvable(start, board) {
-		result := a.AStar(&root, &goal, npuzzle.Greedy, npuzzle.Heuristic)
-		if result.Nodes != nil {
-			// for _, node := range result.Nodes {
-			// 	displayState(&npuzzle, node)
-			// }
+		a.AStar(&root, &goal, npuzzle.Greedy, npuzzle.Heuristic)
+		if a.Path != nil {
 			if npuzzle.JsonOutput {
-				algo.OutputToJson(result.Nodes, goal.State)
+				algo.OutputToJson(a.Path, goal.State)
 			}
-			fmt.Fprintf(npuzzle.Output, "Time complexity: %d nodes in memory\n", result.TimeComplex)
-			fmt.Fprintf(npuzzle.Output, "Size complexity: %d nodes evaluated\n", result.SizeComplex)
-			fmt.Fprintf(npuzzle.Output, "Moves required: %d\n", len(result.Nodes))
+			fmt.Fprintf(npuzzle.Output, "Time complexity: %d nodes in memory\n", a.TimeComplex)
+			fmt.Fprintf(npuzzle.Output, "Size complexity: %d nodes evaluated\n", a.SizeComplex)
+			fmt.Fprintf(npuzzle.Output, "Moves required: %d\n", len(a.Path))
 			return
 		}
 	}
